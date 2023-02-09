ddcb9141f13b9fa95607b30a859ca364b08ac478
AldairUAM-Azc <108377001+AldairUAM-Azc@users.noreply.github.com>
2023-02-08 19:02:32 -0600

    Proyecto1

---
+++ Proyecto1/src/pack/PrimerEjemplo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pack;
+
+/**
+ *
+ * @author ALUMNO32
+ */
+public class PrimerEjemplo {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo!");
+        int a = 3;
+        String c = "LOL";
+        System.out.println("A = " + a);
+        Integer b = 2;
+        int length = c.length();
+    }
+}
